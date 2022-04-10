1c6c1a35b293fdf6e19b2ae843ee99429d1d447e
mcapris <madalin.capris@sorted.com>
2022-04-10 18:08:53 +0300

    endava submarine template

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace EndavaSubmarine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("input.txt");
+
+            //TODO: code goes here
+        }
+    }
+}
